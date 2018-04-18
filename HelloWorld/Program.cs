6ac5ab33ae129fc4506f21707589144b7d544bf2
go-PC\go <esraa.ali.fci@gmail.com>
2018-04-19 00:53:28 +0200

    update file after merge

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,11 +12,9 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello FCI");
             Console.WriteLine("Hello Merge");
-<<<<<<< HEAD
-            Console.WriteLine("This is copy 2");
-=======
             Console.WriteLine("this is Copy 1");
->>>>>>> 012d4bcb5e7a7448af33e4b861fce96de0baf6bb
+            Console.WriteLine("This is copy 2");
+            
             Console.ReadLine();
         }
     }
